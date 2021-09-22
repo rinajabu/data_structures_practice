ea15bd3b97bd9a6e34d1451a5ac09ae0c212f8d3
Rina Joy Abu <rinajoyyy@Rinas-MacBook-Pro.local>
2021-09-21 23:26:27 -0400

    classes overview

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
+///////////////////////////////
 //////////// CLASSES //////////
+///////////////////////////////
+
+class Student {
+    constructor(firstName, lastName, year) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.year = year;
+        this.tardies = 0;
+        this.scores = [];
+    }
+
+    fullName() {
+        return `Your full name is ${this.firstName} ${this.lastName}.`
+    }
+
+    markLate() {
+        this.tardies += 1;
+        if (this.tardies >= 3) {
+            return "YOU'RE EXPELLED!"
+        }
+        return `${this.firstName} ${this.lastName} has been late ${this.tardies} time/s.`
+    }
+
+    addScore(score) {
+        this.scores.push(score);
+        return `Your scores are ${this.scores}.`
+    }
+
+    calculateAverage() {
+        // let average = 0;
+        // this.scores.map(score => average = average + score);
+        let sum = this.scores.reduce((a,b) => a + b);
+        return sum / this.scores.length;
+    }
+}
+
+let firstStudent = new Student("Rina", "Abu", 4)
+
+// console.log(firstStudent)
+// console.log(firstStudent.fullName());
+// console.log(firstStudent.markLate());
+// console.log(firstStudent.markLate());
+// console.log(firstStudent.markLate());
+console.log(firstStudent.addScore(100));
+console.log(firstStudent.addScore(98));
+console.log(firstStudent.calculateAverage());
